6c9c111a4488c0687a73b5058e9753266a7386a3
duswl2418 <duswl2418@naver.com>
2023-11-08 00:18:13 +0900

    2023년 11월 07일 gsap

--- GSAP/advanced-start/01.Animation Control/1-1.motionPath/main.js
+++ GSAP/advanced-start/01.Animation Control/1-1.motionPath/main.js
@@ -12,12 +12,16 @@ const tiger = $('#tiger');
 const animation = gsap.to(tiger,{
     duration:6,
     motionPath: {
-        path:'.route',
+        path:'#route',
         align:tiger,
     }
-});
+})
 
  // gsap 코어를 위에 두고 그다음에 motionPath를 적어줘야 함.
+// 하나의 트윈은 자동재생이 기본이다.
+//paused는 재생과 정지 상태를 관리해주는 것을 말한다. 멈추면 true, 움직이면 false를 반환함. getter와 setter 방식으로 줄 수도 있음. setter은 ()안에 인수를 넣으면 세팅을 해줌.
+
+// animation.pause() 는 정말 정지만 시켜주는 것.
 
 
-console.log(animation);
+console.log(animation.paused());
