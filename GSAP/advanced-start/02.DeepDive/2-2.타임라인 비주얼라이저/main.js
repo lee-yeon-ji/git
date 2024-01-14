12da4aa3aa9b236b6d66c34cd27482dbe2562f40
duswl2418 <duswl2418@naver.com>
2024-01-14 22:37:34 +0900

    gsap 타임라인 비주얼 - startTime() 메서드 재생되는 시간을 알려주는것. - getChildren 메서드 tween의 값을 배열로 보여줌. - svg 일 때는 너비값을 지정해 줘야 한다는데 이유는?

--- GSAP/advanced-start/02.DeepDive/2-2.타임라인 비주얼라이저/main.js
+++ GSAP/advanced-start/02.DeepDive/2-2.타임라인 비주얼라이저/main.js
@@ -4,6 +4,25 @@ const $ = node => document.querySelector(node);
 
 gsap.to('svg',{autoAlpha:1})
 
+const pixelPerSecond = 200; //rect의 박스의 너비를 지정해 줌. svg일 때
+const animation = gsap.timeline();
+
+animation
+    .to('#star',{duration:1,x:1150})
+    .to('#circle',{duration:2,x:1150})
+    .to('#square',{duration:1,x:1150})
+
+const children = animation.getChildren();
+const childrenList = children.length
+
+for(let i = 0; i < childrenList; i++){
+    // console.log( children[i].startTime());
+
+    gsap.set('#tween' + i,{x:children[i].startTime() * pixelPerSecond})
+    gsap.set('#rect' + i,{width:children[i].duration() * pixelPerSecond})
+    //rect 박스의 길이를 늘려주기 위해 곱하기를 해서
+
+}
 
 
 
@@ -11,15 +30,8 @@ gsap.to('svg',{autoAlpha:1})
 
 
 
-
-
-
-
-
-
-
-
-
+//startTime() 메서드는 재생되는 시간을 알려주는 것.
+//getChildren 메서드는 tween의 값을 배열로 보여줌.
 
 
 
