c45d6ed55336c95cec9ff376f723ea55af5b5a59
duswl2418 <duswl2418@naver.com>
2024-01-28 17:39:15 +0900

    gsap 효과 등록하기 -타임라인 자체에 내가 만든 애니메이션을 간편하게 등록해서 사용할 수 있음.

--- GSAP/advanced-start/02.DeepDive/2-5.효과 등록하기_registerEffect/main.js
+++ GSAP/advanced-start/02.DeepDive/2-5.효과 등록하기_registerEffect/main.js
@@ -1,11 +1,47 @@
 
-gsap.registerPlugin(GSDevTools, SplitText);
+// gsap.registerPlugin(GSDevTools, SplitText);
 
 
 
 gsap.set('.stage',{autoAlpha:1})  
 
+gsap.registerEffect({
+    name:'textEffect',
+    extendTimeline:true, //타임라인을 확장하는 것으로 밑에 add부터해서 블라블라 써주지 않아도 됨.
+    defaults:{
+        y:-100,
+        colors:['red','orange']
+    },
+
+    effect:(target,config)=>{
+
+    const split = new SplitText(target,{type:'chars'});
 
 
+        const tl = gsap.timeline();
+        tl.from(split.chars,{y:config.y,opacity:0,stagger:0.05})
+            .to(split.chars,{color:gsap.utils.wrap(config.colors)})
+
+        return tl; // 타임라인 자체를 리턴하게 되면 만든 타임라인이 리턴 됨. 이 타임라인을 가지고 순차적으로 애니메이션을 추가 할 수 있음.
+    }
+})
 
 
+const animation = gsap.timeline(); // animation이라는 큰 타임라인 안에 애니메이션을 추가해서 쓸 수 있음.
+
+animation.textEffect('h1')
+            .textEffect('h2',{y:'random(-100,100)',colors:['blue','green']})
+
+//
+// animation.add(gsap.effects.textEffect('h1'))
+// animation.add(gsap.effects.textEffect('h2',{y:100,colors:['blue','green']}))
+
+
+// gsap.effects.textEffect('h1')
+// gsap.effects.textEffect('h2',{y:100,colors:['blue','green']})
+
+//
+// const tl = gsap.timeline();
+// tl.from(split.chars,{y:-100,opacity:0,stagger:0.05})
+//     .to(split.chars,{color:gsap.utils.wrap(['red','orange'])})
+//
