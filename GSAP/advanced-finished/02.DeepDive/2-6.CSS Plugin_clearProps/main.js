21f938e251a56995f9c4a2452032bc1724104d35
zzya <duswl2418@naver.con>
2024-02-10 18:21:27 +0900

    gsap에 clearprops에 대해 배움

--- GSAP/advanced-finished/02.DeepDive/2-6.CSS Plugin_clearProps/main.js
+++ GSAP/advanced-finished/02.DeepDive/2-6.CSS Plugin_clearProps/main.js
@@ -5,27 +5,15 @@ const $ = node => document.querySelector(node);
 
 
 
-// const boxes = document.querySelectorAll('.box');
-// const boxArray = Array.prototype.slice.call(boxes);
 const boxes = gsap.utils.toArray('.box');
-
+console.log(boxes)
+//유사배열로 나오기 때문에 일반적인 배열의 능력을  사용할 수 없음.
 boxes.forEach((item)=>{
-  
-  item.addEventListener('click',(e)=>{
-    gsap.to(e.currentTarget,{
-      backgroundColor:'gray',
-      width:'300px',
-      x:200,
-      scale:0.3,
-   
+    item.addEventListener('click',(e)=>{
+        gsap.to(e.currentTarget,{
+            backgroundColor:'gray',
+            width:'300px'
+        })
     })
-  })
 })
-
-
-$('#reset').addEventListener('click',()=>{
-  gsap.set('.box',{clearProps:'scale'})
-})
-
-
-
+//gsap에 제공해주는 기능으로 한번에 DOM을 잡아주는 utils 함수의 toArray를
