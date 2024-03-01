278e0015a7da92b40ccd0f175c0366f74059053f
lee-yeon-ji <duswl2418@naver.com>
2024-03-01 18:54:00 +0900

    gsap 유틸 메서드 distribute

--- GSAP/advanced-start/03.Advenced Stagger/3-2.가속도 기반의 Stagger/main.js
+++ GSAP/advanced-start/03.Advenced Stagger/3-2.가속도 기반의 Stagger/main.js
@@ -13,5 +13,11 @@
 gsap.from('.stage > div',{
         duration:2,
         opacity:0,
-        stagger:0.2
+        scale:0,
+        ease:'power3.inOut', //scale과 opacity에 관한 가속도로 stagger 안에서 주는 가속도와는 다름.
+        stagger:{
+                each:0.2,
+                ease:'power3', //가속도를 none, linear의 기본 속성 외에 추가적으로 속성을 추가해서 다양한 효과를 줄 수 있음.
+                from:'center'
+        }
 })
--- GSAP/advanced-start/03.Advenced Stagger/3-3.유틸메서드_distribute/main.js
+++ GSAP/advanced-start/03.Advenced Stagger/3-3.유틸메서드_distribute/main.js
@@ -8,3 +8,23 @@
 
 
 
+//distribute는 유틸 메서드는 배열 또는 그리드의 항목 전체에 값을 배포할 때 사용됨.
+
+const value = gsap.utils.distribute({
+    base:0,
+    amount:400,
+    ease:'power3',
+    from:'center'
+})
+
+gsap.to('.bar',{
+    height:value,
+    duration:2,
+    stagger:{
+        each:0.1,
+
+    }
+})
+
+
+GSDevTools.create()
