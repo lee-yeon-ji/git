a0584a74e0cb4064785c74c3051e8b94c92cb2aa
lee-yeon-ji <duswl2418@naver.com>
2024-03-05 23:39:03 +0900

    -gsap 3D 텍스트 효과 gsap에 기본 transformOrigin에서 값을 다시 잡아줄 수 있음. -동일하게 관리되는 애들은 변수를 따로 빼서 쓸 수 있음. ex) const test = 1

--- GSAP/advanced-start/03.Advenced Stagger/3-5.3D 텍스트 효과/main.js
+++ GSAP/advanced-start/03.Advenced Stagger/3-5.3D 텍스트 효과/main.js
@@ -1,4 +1,24 @@
 
 
 
+const duration = 1 //동일하게 관리되는 애들은 변수를 따로 빼서 쓰는 것이 좋음. stagger에 변수를 넣어준걸 볼 수 있음.
 
+const tl = gsap.timeline();
+
+gsap.set('.utils > div',{transformOrigin: '50% 50% -50'}) //x,y,z 순으로 되어있음.
+
+
+tl.from('.utils > div',{
+    rotationX:-90,
+    rotationY:-90,
+    opacity:0,
+    stagger: duration
+})
+
+.to('.utils > div',{
+    rotationX:90,
+    rotationY:90,
+
+    opacity: 0,
+    stagger: duration
+},duration)// 포지션 파라미터에다 모든 타임라인에 0.5초를 절대값으로 주면 그 시간에 실행되게끔 해줌.
