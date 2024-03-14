52d5c91fb85dd3e99d34b2f54f9310941081e4a7
lee-yeon-ji <duswl2418@naver.com>
2024-03-14 23:59:39 +0900

    gsap

--- GSAP/advanced-start/03.Advenced Stagger/3-7.콜백 안에서의 Stagger/main.js
+++ GSAP/advanced-start/03.Advenced Stagger/3-7.콜백 안에서의 Stagger/main.js
@@ -10,59 +10,56 @@
 
 
 
-gsap.to('.tiger> div',{
-  y:100,
+// gsap.to('.tiger> div',{
+//   y:100,
+//
+//   stagger:{
+//     each:0.2,
+//     repeat:1,
+//     yoyo:true,
+//     onComplete(){
+//       gsap.to(this.targets()[0],{
+//         rotation:360
+//       })
+//     }
+//   }
+// })
+//
+//
+//
+//
+//
+// const l = 8 * 13;
+//
+// for(let i = 0; i < l; i++){
+//   let template = /* html */`
+//     <div class="box"></div>
+//   `
+//   document.querySelector('.stage')?.insertAdjacentHTML('beforeend',template)
+//
+// }
+//
+//위에는 타이거를 움직이게 하는 코드//
 
+
+const {chars,lines,words} = new SplitText('.word > div')
+
+gsap.from(chars,{
+  opacity:0,
+  duration:2,
   stagger:{
-    each:0.2,
-    repeat:1,
-    yoyo:true,
+    each:0.1,
+    from:'random',
+    ease:'power1', //점점 빠르게 나오는 해주는 가속도//
     onComplete(){
       gsap.to(this.targets()[0],{
-        rotation:360
+        delay:0.5,
+        duration: 2,
+        color:'#51ff00'
       })
-    }
+    },
   }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const l = 8 * 13;
-
-for(let i = 0; i < l; i++){
-  let template = /* html */`
-    <div class="box"></div>
-  `
-  document.querySelector('.stage')?.insertAdjacentHTML('beforeend',template)
-
-}
-
-
-
-
-
-
-
-
-
 
 
 
