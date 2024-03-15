98898df388d3893e3c738fc57f3dedf7be07cd24
lee-yeon-ji <duswl2418@naver.com>
2024-03-15 20:02:29 +0900

    gsap stagger 활용 2

--- GSAP/advanced-start/03.Advenced Stagger/3-7.콜백 안에서의 Stagger/main.js
+++ GSAP/advanced-start/03.Advenced Stagger/3-7.콜백 안에서의 Stagger/main.js
@@ -29,56 +29,68 @@
 //
 //
 //
-// const l = 8 * 13;
-//
-// for(let i = 0; i < l; i++){
-//   let template = /* html */`
-//     <div class="box"></div>
-//   `
-//   document.querySelector('.stage')?.insertAdjacentHTML('beforeend',template)
-//
-// }
-//
+
 //위에는 타이거를 움직이게 하는 코드//
 
 
-const {chars,lines,words} = new SplitText('.word > div');
-const tl = gsap.timeline();
+// const {chars,lines,words} = new SplitText('.word > div');
+// const tl = gsap.timeline();
+//
+//
+//
+// tl.from(chars,{
+//   opacity:0,
+//   duration:2,
+//   stagger:{
+//     each:0.1,
+//     from:'random',
+//     ease:'power1', //점점 빠르게 나오는 해주는 가속도//
+//     onComplete(){
+//       gsap.to(this.targets()[0],{
+//         delay:0.5,
+//         duration: 0,
+//         color:'#51ff00'
+//       })
+//     },
+//   }
+// })
+//
+// .to(lines,{
+//   delay:1, //from에서 애니메이션을 시작할 때 OnComPlete가 끝날 때까지 기다려 주지 않기 때문에 delay이를 줘서 위에 애니메이션이 끝난 뒤에 실행 할 수 있도록 해줘야함.//
+//   y:30,
+//   opacity:0,
+//   stagger:{
+//     each:0.3,
+//     from:'end'
+//   }
+// })
+//두번 째 word를 움직이게 하는 코드//
 
 
 
-tl.from(chars,{
-  opacity:0,
-  duration:2,
+
+
+const l = 8 * 13;
+
+for(let i = 0; i < l; i++){
+  let template = /* html */`
+    <div class="box"></div>
+  `
+  document.querySelector('.stage')?.insertAdjacentHTML('beforeend',template)
+
+}
+
+
+gsap.to('.box',{
+  scale:0.3,
+
   stagger:{
-    each:0.1,
-    from:'random',
-    ease:'power1', //점점 빠르게 나오는 해주는 가속도//
-    onComplete(){
-      gsap.to(this.targets()[0],{
-        delay:0.5,
-        duration: 0,
-        color:'#51ff00'
-      })
-    },
+    each0:0.5,
+    onStart(){
+      console.log('start');
+    }
   }
 })
-
-.to(lines,{
-  delay:1, //from에서 애니메이션을 시작할 때 OnComPlete가 끝날 때까지 기다려 주지 않기 때문에 delay이를 줘서 위에 애니메이션이 끝난 뒤에 실행 할 수 있도록 해줘야함.//
-  y:30,
-  opacity:0,
-  stagger:{
-    each:0.3,
-    from:'end'
-  }
-})
-
-
-
-
-
-
 
 
 
