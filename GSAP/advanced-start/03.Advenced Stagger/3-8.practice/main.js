b0af1e4235dc09f3e299c5694078b7e371d6b4a4
lee-yeon-ji <duswl2418@naver.com>
2024-05-02 18:03:38 +0900

    gsap 행성

--- GSAP/advanced-start/03.Advenced Stagger/3-8.practice/main.js
+++ GSAP/advanced-start/03.Advenced Stagger/3-8.practice/main.js
@@ -15,29 +15,135 @@ const planet = [
 
 
 
-
-// map  -> 배열의 능력 (새로운 배열을 반환)
-// forEach -> 배열의 능력 (값을 반환하지 x)
-// insertAdjacentHTML (dom 뿌려주는)
-
-
-const todo = [
-  '밥',
-  '일',
-  '게임',
-  '코딩'
-    ]
-
-
-
-const list = todo.map((item,index)=>{
-  return `<div class="${index}">${item}</div>`
-}) //map의 형식//
-
-
-list.forEach((item,index)=>{
-  console.log(item,index);
+const list = planet.map((planetName)=>{
+  return /* html */ `
+    <div class="solar_system" data-planet-name="${planetName}">
+      <div class="planet ${planetName}">
+        <div class="overlay"></div>
+        <h2>${planetName}</h2>
+      </div>
+    </div>
+  `
 })
 
 
 
+const space = $('.space')
+
+list.forEach((planet)=>{
+  space.insertAdjacentHTML('beforeend',planet)
+
+})
+
+function render(){
+  const space = $('.space')
+  list.forEach((planet)=>{
+    space.insertAdjacentHTML('beforeend',planet)
+  })
+}
+
+render();
+
+const z = gsap.utils.distribute({
+  base: -18400,
+  amount: 18400,
+  from: 'end'
+})
+
+
+
+gsap.set('.planet',{
+  z:z,
+  scaleX:0.8,
+  rotateX:4,
+
+})
+
+const rightButton = $('.right');
+
+let count = 0;
+let trigger = false;
+
+rightButton.addEventListener('click',()=>{
+
+  if(count > 7) return;
+
+
+
+  if(!trigger){
+    ++count;
+
+
+    gsap.to('.planet',{
+      z:'+=1000',
+      ease:'power3.inOut',
+      duration:2,
+      onComplete(){
+        trigger = false;
+      }
+    })
+
+    trigger = true;
+  }
+
+
+
+})
+
+
+
+
+const leftButton = $('.left');
+
+leftButton.addEventListener('click',()=>{
+
+  if(count < 1) return;
+
+  if(!trigger){
+    --count;
+
+
+    gsap.to('.planet',{
+      z:'-=1000',
+      ease:'power3.inOut',
+      duration:2,
+      onComplete(){
+        trigger = false;
+      }
+    })
+
+    trigger = true;
+  }
+
+
+
+
+
+})
+
+
+
+//
+// map  -> 배열의 능력 (새로운 배열을 반환)
+// forEach -> 배열의 능력 (값을 반환하지 x)
+// insertAdjacentHTML (dom 뿌려주는)
+//
+//
+// const todo = [
+//   '밥',
+//   '일',
+//   '게임',
+//
+//
+//
+// const list = todo.map((item,index)=>{
+//   return `<div class="${index}">${item}</div>`
+// }) //map의 형식//
+//
+//
+// list.forEach((item,index)=>{
+//   console.log(item,index)
+// })
+//
+//
+//
