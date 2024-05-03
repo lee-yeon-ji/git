b1e9e89a27bfd6030ad58d4f1ce668b17044d139
lee-yeon-ji <duswl2418@naver.com>
2024-05-03 23:55:39 +0900

    gsap 순차적 애니메이션 제어하기

--- GSAP/advanced-start/04.Advanced Timeline/4-1.순차적 애니메이션 제어/main.js
+++ GSAP/advanced-start/04.Advanced Timeline/4-1.순차적 애니메이션 제어/main.js
@@ -3,7 +3,20 @@ const $ = node => document.querySelector(node);
 
 
 
+const tl = gsap.timeline({
+    defaults:{
+        scale:0,
+        opacity:0,
+        duration:1
+    }
+});
 
+
+tl.from('.tiger[data-name="a0"]',{})
+tl.from('.tiger[data-name="a0"]',{})
+tl.from('.tiger[data-name="a0"]',{})
+tl.from('.tiger[data-name="a0"]',{})
+tl.from('.tiger[data-name="a0"]',{})
 
 
 
