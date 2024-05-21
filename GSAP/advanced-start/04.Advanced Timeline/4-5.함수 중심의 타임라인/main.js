2b8f280ceb0caf0a7c5981ffa02c9e301530aa72
lee-yeon-ji <duswl2418@naver.com>
2024-05-21 23:54:43 +0900

    gsap 함수 중심의 텍스트 만들기

--- GSAP/advanced-start/04.Advanced Timeline/4-5.함수 중심의 타임라인/main.js
+++ GSAP/advanced-start/04.Advanced Timeline/4-5.함수 중심의 타임라인/main.js
@@ -25,35 +25,54 @@ gsap.defaults({
 //   }
 // })
 
-
-const orangeAnimation = gsap.timeline()
-.to(orange,{scale:2})
-.to(orange,{rotation:360})
-.to(orange,{scale:1})
-.to([green,quote],{y:0,stagger:0.2,repeat:1,yoyo:true,repeatDelay:1})
-
-
-const blueAnimation = gsap.timeline()
-.to(blue,{y:150})
-.to(blue,{rotation:360})
-.to(blue,{y:0}) 
-.to([green,quote],{y:0,stagger:0.2,repeat:1,yoyo:true,repeatDelay:1})
+function tigerAnimation(){
+  const orangeAnimation = gsap.timeline()
+      .to(orange,{scale:2})
+      .to(orange,{rotation:360})
+      .to(orange,{scale:1})
 
 
-const pinkAnimation = gsap.timeline()
-.to(pink,{scale:0.5}) 
-.to(pink,{rotation:-360}) 
-.to(pink,{scale:1}) 
-.to([green,quote],{y:0,stagger:0.2,repeat:1,yoyo:true,repeatDelay:1})
+  const blueAnimation = gsap.timeline()
+      .to(blue,{y:150})
+      .to(blue,{rotation:360})
+      .to(blue,{y:0})
 
 
 
+  const pinkAnimation = gsap.timeline()
+      .to(pink,{scale:0.5})
+      .to(pink,{rotation:-360})
+      .to(pink,{scale:1})
+
+
+  return [orangeAnimation,blueAnimation,pinkAnimation]
+
+
+}
+//배열 구조 분해 할당//
+const [orangeA,blueA,pinkA] = tigerAnimation();
+// const orangeA = tigerAnimation()[0]
+// const blueA = tigerAnimation()[1]
+// const pinkA = tigerAnimation()[2]
+
+
+
+function quoteAnimation(message){
+  const tl = gsap.timeline()
+  .set(quote,{text:message})
+  .to([green,quote],{y:0,stagger:0.2,repeat:1,yoyo:true,repeatDelay:1})
+
+  return tl
+}
 
 
 const master = gsap.timeline()
-.add(orangeAnimation)
-.add(blueAnimation)
-.add(pinkAnimation)
+.add(orangeA)
+.add(quoteAnimation('orange tiger!'))
+.add(blueA)
+.add(quoteAnimation('blue tiger!'))
+.add(pinkA)
+.add(quoteAnimation('pink tiger!'))
 
 
 
